fb564101e32cd0ae453b8226013c53836290596a
Palash Chauhan <palashc983@gmail.com>
2016-08-16 17:03:11 +0530

    used lemmatizer, stemmer, pos-tagger

--- Assignments/A1/tokenizer.py
+++ Assignments/A1/tokenizer.py
@@ -1,6 +1,8 @@
 import libhfst
 from sklearn.datasets import fetch_20newsgroups
 import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import *
 
 
 
@@ -9,8 +11,17 @@ newsgroups_train = fetch_20newsgroups(subset='train')
 
 
 t = nltk.word_tokenize(str(newsgroups_train.data[0]))
-s = nltk.sent_tokenize(str(newsgroups_train.data[0]))
 
-print t
-print s
+# s = nltk.sent_tokenize(str(newsgroups_train.data[0]))
+
+tags = nltk.pos_tag(t)
+tags = dict(tags)
+# print t
+# print s
+# print tags
+
+WNL = WordNetLemmatizer()
+porter_stemmer = PorterStemmer()
+for word in t:
+	print word + " ---> " + WNL.lemmatize(word) + " ---> " + porter_stemmer.stem(word) + " ---> " + tags[word]
 
